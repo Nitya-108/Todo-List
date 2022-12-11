2000c08a55b8c5613ed6592854dfbdcfc2d42c09
Nitya-108 <clinton.moodley@protonmail.com>
2022-12-11 16:28:13 +0530

    added delete button function on js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector(".todo-list");
 
 // Event listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 // Functions
 function addTodo(event){
@@ -32,4 +33,12 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
     // CLEAR TODO INPUT VALUE
     todoInput.value ="";
+}
+
+function deleteCheck (e) {
+    const item = e.target;
+// DELETE TODO
+    if(item.classList[0] === "trash-btn");{
+    item.remove();
+    }
 }
